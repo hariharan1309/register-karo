91cfcc470ebcc0fbe08f61f169e37ac3e916c4c7
hariharan1309 <63937651+hariharan1309@users.noreply.github.com>
2025-03-08 21:29:52 +0530

    Why Section Improvements

--- src/components/WhyCard.tsx
+++ src/components/WhyCard.tsx
@@ -1,8 +1,8 @@
-
 const WhyCard = ({ params }: { params: any }) => {
   return (
     <div
-      className={`px-5 py-9 rounded  flex flex-col border-[#FCDDEC] border-[1px] items-center gap-5 *:font-inter`}
+      className={`px-5 py-[30px] rounded-[10px]  flex flex-col border-[#FCDDEC] border-[1px] items-center gap-5 *:font-inter`}
+      style={{ backgroundColor: params?.color || "" }}
     >
       {params.icon}{" "}
       <h4 className="font-medium text-xl leading-[30px]">{params.title}</h4>
